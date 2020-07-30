9591cea09b9da7070c83c84b835124e4b99bfdba
lhr519 <lhr519@naver.com>
2020-07-30 21:53:11 +0900

    Delete hello2.py

--- hello2.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient('localhost', 27017)
-db = client.dbsparta
-
-target_movie = db.movies.find_one({'title': '월-E'})
-target_star = target_movie['star']
-
-db.movies.update_many({'star': target_star}, {'$set': {'star': '0'}})

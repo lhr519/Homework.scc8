8dcab369c0546202791ef65ad4b91f790c453b99
lhr519 <lhr519@naver.com>
2020-07-30 21:50:29 +0900

    Update hello2.py

--- hello2.py
+++ hello2.py
@@ -1,12 +1,9 @@
-import requests
-from bs4 import BeautifulSoup
-from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+from pymongo import MongoClient
 
-client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
-db = client.dbsparta  # 'dbsparta'라는 이름의 db를 만듭니다.
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
 
+target_movie = db.movies.find_one({'title': '월-E'})
+target_star = target_movie['star']
 
-star = db.movies2.find_one({'title':'월-E'})['star']
-stars = list(db.movies2.find({'star':star}))
-db.movies2.update_many({'star': star},{'$set': {'star': '0'}})
-print(stars)
+db.movies.update_many({'star': target_star}, {'$set': {'star': '0'}})
